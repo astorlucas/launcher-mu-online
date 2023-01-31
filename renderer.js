0e148a3df9a906a5fbe307334d6854adc6945c4d
astorlucas <lucasastor@gmail.com>
2023-01-31 17:39:51 -0300

    cannot run launcher properly

--- renderer.js
+++ renderer.js
@@ -1,5 +1,29 @@
 function executeMain() {
-  window.alert("ALERTA");
-  var shell = new ActiveXObject("WScript.shell");
-  shell.run("notepad.exe", 1, True);
+  // var child = require("child_process").execFile;
+  // var executablePath = "CLIENTE BASE\\main.exe";
+
+  // child(executablePath, function (err, data) {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
+
+  //   console.log(data.toString());
+  // });
+  const path = require("CLIENTE BASE\\main.exe");
+  const { execFile } = require("child_process");
+
+  describe("cli test", () => {
+    test("thing", () => {
+      const myCli = execFile(
+        `sh ${path.resolve(__dirname, "..")}/cli.js`,
+        ["--foo", "Bar"],
+        (err, stdout, stderr) => {
+          if (err) {
+            console.log("err: ", err);
+          }
+        }
+      );
+    });
+  });
 }
