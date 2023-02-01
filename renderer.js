d602a2a03313f0d4a60434f147b6db4c65ab559c
astorlucas <lucasastor@gmail.com>
2023-02-01 11:50:58 -0300

    bg added cannot open app

--- renderer.js
+++ renderer.js
@@ -1,29 +1,5 @@
 function executeMain() {
-  // var child = require("child_process").execFile;
-  // var executablePath = "CLIENTE BASE\\main.exe";
-
-  // child(executablePath, function (err, data) {
-  //   if (err) {
-  //     console.error(err);
-  //     return;
-  //   }
-
-  //   console.log(data.toString());
-  // });
-  const path = require("CLIENTE BASE\\main.exe");
-  const { execFile } = require("child_process");
-
-  describe("cli test", () => {
-    test("thing", () => {
-      const myCli = execFile(
-        `sh ${path.resolve(__dirname, "..")}/cli.js`,
-        ["--foo", "Bar"],
-        (err, stdout, stderr) => {
-          if (err) {
-            console.log("err: ", err);
-          }
-        }
-      );
-    });
-  });
+  var oShell = new ActiveXObject("Shell.Application");
+  var commandtoRun = "C:\\Windows\\notepad.exe";
+  oShell.ShellExecute(commandtoRun, "", "", "open", "1");
 }
